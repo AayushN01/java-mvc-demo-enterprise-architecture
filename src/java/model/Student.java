84773d1c90d80fe505c861a6dce11fc1e2469488
AayushN01 <aayushniraula1@gmail.com>
2024-12-09 20:19:42 +0545

    model definition

---
+++ src/java/model/Student.java
@@ -0,0 +1,62 @@
+package model;
+
+public class Student {
+
+    private Integer id;
+    private String name;
+    private String gender;
+    private Integer age;
+    private String roll_no;
+    private String address;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getRoll_no() {
+        return roll_no;
+    }
+
+    public void setRoll_no(String roll_no) {
+        this.roll_no = roll_no;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+
+    
+}
---
+++ src/java/model/StudentCourse.java
@@ -0,0 +1,24 @@
+package model;
+
+public class StudentCourse {
+
+    private Integer std_id;
+    private Integer course_id;
+
+    public Integer getStd_id() {
+        return std_id;
+    }
+
+    public void setStd_id(Integer std_id) {
+        this.std_id = std_id;
+    }
+
+    public Integer getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(Integer course_id) {
+        this.course_id = course_id;
+    }
+
+}
